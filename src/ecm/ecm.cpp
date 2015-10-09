ff39d3c9d52f003ec5273759c79efdbf82ad801c
Daniel Lerch Hostalot <dlerch@gmail.com>
2015-10-09 19:04:22 +0200

    minor changes

--- src/ecm/ecm.cpp
+++ src/ecm/ecm.cpp
@@ -1,7 +1,7 @@
 /*
    Copyright 2015 Daniel Lerch Hostalot.
 
-   This file is part of FactorECM.
+   This file is part of factorlab.
 
    factorlab is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
