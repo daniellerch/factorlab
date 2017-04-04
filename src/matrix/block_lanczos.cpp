bf9e774a4964afcd290db4c2a97279d93f462297
dlerch <dlerch@gmail.com>
2017-04-04 22:53:03 +0200

    fix copy/paste error in license

--- src/matrix/block_lanczos.cpp
+++ src/matrix/block_lanczos.cpp
@@ -10,13 +10,13 @@
    the Free Software Foundation; either version 2 of the License, or            
    (at your option) any later version.                                          
                                                                                 
-   FLINT is distributed in the hope that it will be useful,                     
+   factorlab is distributed in the hope that it will be useful,                     
    but WITHOUT ANY WARRANTY; without even the implied warranty of               
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
    GNU General Public License for more details.                                 
                                                                                 
    You should have received a copy of the GNU General Public License            
-   along with FLINT; if not, write to the Free Software                         
+   along with factorlab; if not, write to the Free Software                         
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA 
 */
 
@@ -30,7 +30,7 @@
                         --jasonp@boo.net 9/8/06
     
 
-   Note from FLINT code:                    
+   Note from factorlab code:                    
    =====================
 
    The following modifications were made by William Hart:
